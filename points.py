7581b6439ecb64f4c8da06d5cabd1987e15e467a
kachan18 <65244974+kachan18@users.noreply.github.com>
2021-03-07 21:18:28 +0900

    Add files via upload

--- points.py
+++ points.py
@@ -215,11 +215,9 @@ async def earningtyping(message, args, authinfo, dbpass):
 
 
 def makerandomstring():
-    stringlist = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-                  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    stringlist = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     res = []
-    for i in range(0, random.randint(5, 15)):
+    for i in range(0, random.randint(5, 20)):
         res.append(random.choice(stringlist))
     res = "".join(res)
     return res
